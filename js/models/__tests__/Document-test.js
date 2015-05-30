62f47b35ac6475c4c993f80103d094f3769e0da9
hackerwins <susukang98@gmail.com>
2015-05-30 13:53:09 +0900

    Add jest for test.

---
+++ js/models/__tests__/Document-test.js
@@ -0,0 +1,9 @@
+jest.dontMock('../Document.js');
+
+describe('Document', function () {
+  var Document = require('../Document.js');
+
+  it('find node position with offset', function () {
+    expect(true).toEqual(true);
+  });
+});
